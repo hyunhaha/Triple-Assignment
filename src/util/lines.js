a534d1d93472628dc71a9ac6c19b3d36872a9534
hyunhaha <gusgk2658@naver.com>
2022-06-20 04:48:43 +0900

    chore: 불필요한 코드 삭제

--- src/util/lines.js
+++ src/util/lines.js
@@ -7,11 +7,7 @@ export const countUpLines = [
   '100만 개의 여행 리뷰',
   '470만 개의 여행 일정',
 ]
-//   [
-//   [700, '만 명', '의 여행자'],
-//   [100, '만 개', '의 여행 리뷰'],
-//   [470, '만 개', '의 여행 일정'],
-// ]
+
 export const awardData = [
   {
     image: 'images/play-store2x.png',
