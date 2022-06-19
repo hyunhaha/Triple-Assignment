05d600cb271f0d389c3c46ab6c143a38c4a4fa33
hyunhaha <gusgk2658@naver.com>
2022-06-19 18:11:07 +0900

    feat: 영역별 등장 애니메이션 컴포넌트 추가

--- src/Component/AppIntroduce.js
+++ src/Component/AppIntroduce.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import AppearAnimation from './AppearAnimation'
 
 const AppIntroduce = (props) => {
   return (
     <div>
-      <div>2021년 12월 기준</div>
-      <div>
+      <AppearAnimation duration={700} delay={100}>
+        2021년 12월 기준
+      </AppearAnimation>
+      <AppearAnimation duration={700} delay={200}>
         <div>700만 명의 여행자</div>
         <div>100만 개의 여행 리뷰</div>
         <div>670만 개의 여행 일정</div>
-      </div>
-      <div>
+      </AppearAnimation>
+      <AppearAnimation duration={700} delay={300}>
         <div>
           <div> 2018 구글 플레이 스토어</div>
           <div>올해의 앱 최우수상 수상</div>
@@ -18,9 +21,8 @@ const AppIntroduce = (props) => {
           <div>2018 애플 앱스토어</div>
           <div>오늘의 여행앱 선정</div>
         </div>
-      </div>
+      </AppearAnimation>
     </div>
   )
 }
-
 export default AppIntroduce
